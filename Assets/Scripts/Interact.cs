3ecd5228cb8578c549749c2629188831c33b5bad
pelinakpinar <pelinakpinar96@gmail.com>
2019-04-24 01:08:05 +0300

    initialize

---
+++ Assets/Scripts/Interact.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interact : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string interactButton;
+
+    public float interactDistance = 3f;
+
+    public LayerMask interactLayer;
+
+    public Image interactIcon;
+
+    public bool isInteracting;
+
+    void Start()
+    {
+        if(interactIcon != null)
+        {
+        interactIcon.enabled = false;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(transform.GetChild(0).position, transform.GetChild(0).forward);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, interactDistance, interactLayer))
+        {
+            if(isInteracting == false)
+            {
+                if(interactIcon!= null)
+                {
+                interactIcon.enabled = true;
+                }
+                
+
+                if(Input.GetButtonDown(interactButton))
+                {
+                    if(hit.collider.CompareTag("Note"))
+                    {
+                        hit.collider.GetComponent<Note>().ShowNoteImage();
+                    }
+                   
+                }
+            }
+        }
+        else
+        {
+            interactIcon.enabled = false;
+        }
+       
+
+
+    }
+
+}
---
+++ Assets/Scripts/Note.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Note : MonoBehaviour
+{
+    public Image noteImage;
+    public GameObject hideNoteButton;
+    public GameObject playerObject;
+    //public GameObject cameraObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        noteImage.enabled = false;
+        hideNoteButton.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    public void ShowNoteImage()
+    {
+      noteImage.enabled = true;
+        hideNoteButton.SetActive(true);
+      playerObject.GetComponent<PlayerController>().enabled = false;
+       // playerObject.GetComponent<camMouseLook>().enabled= false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        
+    }
+
+    public void HideNoteImage()
+    {
+        noteImage.enabled = false;
+        hideNoteButton.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+       playerObject.GetComponent<PlayerController>().enabled = true;
+        //playerObject.GetComponent<camMouseLook>().enabled = true;
+    }
+}
