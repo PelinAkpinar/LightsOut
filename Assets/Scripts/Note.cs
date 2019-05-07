50f3a5bdb8a21f683d0b980d83010fee543e9c7c
pelinakpinar <pelinakpinar96@gmail.com>
2019-05-08 01:48:41 +0300

    PauseScene

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -6,10 +6,13 @@ using UnityEngine.UI;
 
 public class Note : MonoBehaviour
 {
+    public AudioClip pickUp;
+    public AudioClip putDown;
     public Image noteImage;
     public GameObject hideNoteButton;
     public GameObject playerObject;
-    
+    public GameObject noteObject;
+
     void Start()
     {
         noteImage.enabled = false;
@@ -18,7 +21,9 @@ public class Note : MonoBehaviour
     
     public void ShowNoteImage()
     {
+        GetComponent<AudioSource>().PlayOneShot(pickUp);
       noteImage.enabled = true;
+       
         hideNoteButton.SetActive(true);
         playerObject.SetActive(false);
         //playerObject.GetComponent<PlayerController>().enabled = false;
@@ -30,11 +35,16 @@ public class Note : MonoBehaviour
 
     public void HideNoteImage()
     {
+       
+       
         noteImage.enabled = false;
-       hideNoteButton.SetActive(false);
+         GetComponent<AudioSource>().PlayOneShot(putDown);
+        hideNoteButton.SetActive(false);
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
        playerObject.SetActive(true);
+        noteObject.SetActive(false);
+       
       // playerObject.GetComponent<PlayerController>().enabled = true;
         
     }
---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class PauseScript : MonoBehaviour
+{
+    GameObject pauseMenu;
+    
+    bool paused;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        paused = false;
+        pauseMenu = GameObject.Find("pauseMenu");
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = !paused;
+        }
+        if(paused==true)
+        {
+            
+            pauseMenu.SetActive(true);
+           
+            Time.timeScale = 0;
+            
+        }
+        else if (paused==false)
+        {
+            
+            pauseMenu.SetActive(false);
+           
+            Time.timeScale = 1;
+        }
+       
+    }
+
+    public void Resume()
+    {
+        paused = false;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Scene_Menu");
+    }
+
+}
